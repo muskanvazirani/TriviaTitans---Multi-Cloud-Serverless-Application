5a19027858d78e6cf6badd7fd53df45ec2d97f71
Shaik Saifuddin <sh450956@dal.ca>
2023-07-14 16:34:38 -0300

    installed react router and layout setup for frontend

---
+++ serverless-trivia-frontend/src/components/layout/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header-title">Serverless Trivia Game</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ serverless-trivia-frontend/src/pages/UserAuthentication/UserAuthentication.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserAuthentication = () => {
+  return (
+    <div>UserAuthentication</div>
+  )
+}
+
+export default UserAuthentication
