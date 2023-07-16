e586f715918535ec9bad1288a40518c495fa694e
Shaik Saifuddin <sh450956@dal.ca>
2023-07-16 02:14:20 -0300

    add validet answers lambda

--- serverless-trivia-backend/user-profile-management/handlers/add-user-answers.js
+++ serverless-trivia-backend/user-profile-management/handlers/add-user-answers.js
@@ -11,37 +11,38 @@
 }
  */
 
-const aws = require('aws-sdk');
-aws.config.update({region: 'us-east-1'});
+const aws = require("aws-sdk");
+aws.config.update({ region: "us-east-1" });
 
-const util = require('../utils.js')
+const util = require("../utils.js");
 
 const dynamodb = new aws.DynamoDB.DocumentClient();
 const tableName = process.env.USER_QUS_ANS_TABLE;
 
 exports.handler = async (event) => {
-    try {
-        let item = JSON.parse(event.body).item;
+  try {
+    let item = JSON.parse(event.body).item;
 
-        await dynamodb.put({
-            TableName: tableName,
-            Item: item
-        }).promise()
+    await dynamodb
+      .put({
+        TableName: tableName,
+        Item: item,
+      })
+      .promise();
 
-        return {
-            statusCode: 200,
-            headers: util.getResponseHeaders(),
-            body: JSON.stringify(item)
-        }
-
-    } catch(err) {
-        return {
-            statusCode: err.statusCode ? err.statusCode: 500,
-            header: util.getResponseHeaders(),
-            body: JSON.stringify({
-                error: err.name ? err.name: "Exception",
-                message: err.message ? err.message: "Unknown error"
-            })
-        }
-    }
-}
+    return {
+      statusCode: 200,
+      headers: util.getResponseHeaders(),
+      body: JSON.stringify(item),
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
+      header: util.getResponseHeaders(),
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error",
+      }),
+    };
+  }
+};
